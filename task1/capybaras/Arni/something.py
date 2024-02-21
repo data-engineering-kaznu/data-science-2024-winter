7abe39fef5aa5be8d3e0bdf03347356bfd906fb6
Arnur <agabekovarnur@gmail.com>
2024-02-21 20:00:55 +0600

    MMM

--- task1/capybaras/Arni/something.py
+++ task1/capybaras/Arni/something.py
@@ -1,18 +1,14 @@
 import csv
 data=[]
-
-with open('test.cvs') as csv_file:
+with open('test.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    line_countode
     for row in csv_reader:
-        if line_count == 0:
+        if row[0] == '0':
             data.append(row)
-
-            line_count += 1
         else:
-            data.append([row[0]+'-01-01',row[1],row[2]])
+            data.append([row[0] + '-01-01', row[1], row[2]])
 
-with open('test2.cvs', mode='w',newline='') as csv_file:
+with open('test2.csv', mode='w',newline='') as csv_file:
     csv_writer = csv.writer(csv_file,delimiter=',')
     for i in data:
         csv_writer.writerow(i)
