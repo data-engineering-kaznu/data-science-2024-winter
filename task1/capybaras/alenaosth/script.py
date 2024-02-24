63123c4802882496fa2bfc413203f09acfcd577a
Elena O <ostashova.alena@gmail.com>
2024-02-24 12:32:47 +0600

    fixed

--- task1/capybaras/alenaosth/script.py
+++ task1/capybaras/alenaosth/script.py
@@ -3,9 +3,11 @@ new_data = []
 
 with open('data_old.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
+    total =0
     for i in csv_reader:
-        if i[0] != 'year':
+        if total != 0 : 
             new_data.append([i[0]+'-01-01', i[1], i[2]])
+            total+=1
 
 with open('data_new.csv', mode='w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file, delimiter=',')
