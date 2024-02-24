47c896f7cbf86983b2252100072ae2df85df5112
Arnur <agabekovarnur@gmail.com>
2024-02-24 12:43:59 +0600

    MMM

--- task1/capybaras/Arni/something.py
+++ task1/capybaras/Arni/something.py
@@ -3,7 +3,7 @@ data=[]
 with open('test.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
-        if row[0] == '0':
+        if row[0] == 'year':
             data.append(row)
         else:
             data.append([row[0] + '-01-01', row[1], row[2]])
