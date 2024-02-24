c062adf9a4f0a81acb2992a58292ed7852a4b0b7
T0mpak <musaacuminatagenep@yandex.com>
2024-02-24 13:38:05 +0600

    calculator last commit

--- Serikov_Yelaman/classwork_24_02/calculator.py
+++ Serikov_Yelaman/classwork_24_02/calculator.py
@@ -1,1 +1,46 @@
 print("This is calculator")
+
+def adding(a, b):
+    return a + b
+
+def substracting(a, b):
+    return a - b
+
+def multiplacation(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
+def exponentiation(a, b):
+    return a ** b
+        
+
+def main():
+    a = int(input("Input first operand: "))
+    b = int(input("Input second operand: "))
+    
+    #Adding
+    result_of_adding = adding(a, b)
+    print(result_of_adding)
+
+    #Substacting
+    result_of_substracting = substracting(a, b)
+    print(result_of_substracting)
+    
+    #Multiplication
+    result_of_multiplication = multiplacation(a, b)
+    print(result_of_multiplication)
+    
+    #Division
+    result_of_division = division(a, b)
+    print(result_of_division)
+
+    #Exponentiation
+    result_of_exponentiation = exponentiation(a, b)
+    print(result_of_exponentiation)
+
+    return 0
+
+if __name__ == "__main__":
+    main()
