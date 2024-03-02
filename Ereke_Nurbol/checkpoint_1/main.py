c5c64f53122e995edfd7b7115c0626a476c7955b
LOGKF <erekenurbol@gmail.com>
2024-03-02 14:59:50 +0600

    1

--- Ereke_Nurbol/checkpoint_1/main.py
+++ Ereke_Nurbol/checkpoint_1/main.py
@@ -1,11 +1,8 @@
 import csv
-
 with open('input.csv', 'r') as input_file, open('output.csv', 'w', newline='') as output_file:
     csv_reader = csv.reader(input_file)
     csv_writer = csv.writer(output_file)
-
     csv_writer.writerow(next(csv_reader))
-
     for row in csv_reader:
         row[0] = row[0][4:6] + '-' + row[0][6:] + '-' + row[0][:4]
         row[2] = str(int(row[2]) + int(next(csv_reader)[2]))
