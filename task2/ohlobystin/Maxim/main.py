623247b3a0c208487f1fc19aa3517cfb86b43640
Ssosiska77 <maximkas2305il.com>
2024-03-02 09:34:59 +0600

    task2

---
+++ task2/ohlobystin/Maxim/main.py
@@ -0,0 +1,25 @@
+import csv
+
+months = {
+   'Jan': '01',
+   'Feb': '02',
+   'Mar': '03',
+   'Apr': '04',
+   'May': '05',
+   'Jun': '06',
+   'Jul': '07',
+   'Aug': '08',
+   'Sep': '09',
+   'Oct': '10',
+   'Nov': '11',
+   'Dec': '12'
+}
+
+with open('Input.csv', 'r') as input_csv, open('Output.csv', 'w') as output_csv:
+    reader = csv.reader(input_csv)
+    writer = csv.writer(output_csv)
+    next(reader)
+    writer.writerow(['year', 'reagion', 'value'])
+    for row in reader:
+        row[0] = row[0].split('-')[0] + '-' + months[row[0].split('-')[1]]
+        writer.writerow(row)
